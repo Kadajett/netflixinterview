40aa4edc2aac0743e53d98aae55a529447f71174
Jeremy Stover(Kadajett) <jeremy.ryan.stover@gmail.com>
2014-04-04 02:52:18 -0400

    adds close button to modals so mobile users dont get stuck

--- app/scripts/services/userinputservice.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('netflixinterviewApp')
-  .service('Userinputservice', function Userinputservice() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
